be159a27e3f8b3af856336b68ff4a791aa091923
fjw <fjw@garx.de>
2019-06-06 14:39:32 +0200

    juhu

--- src/World.php
+++ src/World.php
@@ -1,14 +1,13 @@
 <?php
 
-
 namespace Hello;
-
 
 class World
 {
+
     public static function say($what)
     {
-        echo "Hello World, ".$what."!";
+        echo "Hello World, " . $what . "!";
         return "Das ist für dich Sebastian! DU AUCH MARCUS!!";
     }
 }
